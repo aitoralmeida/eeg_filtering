90630b45336825e01ad401d98e0e83faaa0fd970
miguelfrndzdeusto <91140020+miguelfrndzdeusto@users.noreply.github.com>
2023-10-18 16:31:02 +0200

    Added benchmark metrics implementations

---
+++ benchmark_metrics.py
@@ -0,0 +1,60 @@
+import numpy as np
+import scipy.signal
+import pandas as pd
+import seaborn as sns
+import matplotlib.pylab as plt
+from statistics import variance
+
+"""
+Quality assessment metrics for EEG denoising methods
+"""
+
+def covariance(x,y):
+    """ 
+    Returns the covariance between x and y.
+    (Note: For Python > 3.10 the statistics module includes an analogous covariance method)
+    """
+    return np.cov(x,y)[0,1]
+
+def rms(x):
+    """ 
+    Returns the RMS (Root Mean Squared) value of x as defined in eq. 3 of: 
+        ~'Zhang, H., Zhao, M., Wei, C., Mantini, D., Li, Z., & Liu, Q. (2021). 
+        EEGdenoiseNet: a benchmark dataset for deep learning solutions of EEG 
+        denoising. Journal of Neural Engineering, 18(5), 056057.'~
+    """
+    return np.sqrt(np.divide(np.sum(np.power(x,2)), len(x)))
+
+def rrmse_temporal(denoised, ground_truth):
+    """ 
+    Returns the RRMSE (Relative Root Mean Squared Error), in the *temporal* domain, 
+    between the denoised values [f(y)] and the clean groud truths [x] as defined in 
+    eq. 7 of:
+        ~'Zhang, H., Zhao, M., Wei, C., Mantini, D., Li, Z., & Liu, Q. (2021). 
+        EEGdenoiseNet: a benchmark dataset for deep learning solutions of EEG 
+        denoising. Journal of Neural Engineering, 18(5), 056057.'~
+    """
+    return np.divide(rms(denoised - ground_truth), rms(ground_truth))
+
+def rrmse_spectral(denoised, ground_truth, sampling_freq = 256):
+    """ 
+    Returns the RRMSE (Relative Root Mean Squared Error), in the *spectral* domain, 
+    between the denoised values [f(y)] and the clean groud truths [x] as defined in 
+    eq. 8 of:
+        ~'Zhang, H., Zhao, M., Wei, C., Mantini, D., Li, Z., & Liu, Q. (2021). 
+        EEGdenoiseNet: a benchmark dataset for deep learning solutions of EEG 
+        denoising. Journal of Neural Engineering, 18(5), 056057.'~
+    """
+    frequencies_denoised, PSD_denoised = scipy.signal.periodogram(x = denoised, fs = sampling_freq, scaling = 'density')
+    frequencies_ground_truth, PSD_ground_truth = scipy.signal.periodogram(x = ground_truth, fs = sampling_freq, scaling = 'density')
+    return np.divide(rms(PSD_denoised - PSD_ground_truth), rms(PSD_ground_truth))
+
+def cc(denoised, ground_truth):
+    """ 
+    Returns the CC (Correlation Coefficient) between the denoised values [f(y)] 
+    and the clean groud truths [x] as defined in eq. 9 of:
+        ~'Zhang, H., Zhao, M., Wei, C., Mantini, D., Li, Z., & Liu, Q. (2021). 
+        EEGdenoiseNet: a benchmark dataset for deep learning solutions of EEG 
+        denoising. Journal of Neural Engineering, 18(5), 056057.'~
+    """
+    return np.divide(covariance(denoised, ground_truth), np.sqrt(variance(denoised)*variance(ground_truth)))
